6535827d1a5dba3c3a7b5b24e0c2e58e94385f87
Dulshan Chamika <dulshanchamika2@gmail.com>
2025-04-26 16:35:00 +0530

    updatet index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
-
-// import authRoutes from "./routes/authRoutes.js";
-// import bookRoutes from "./routes/bookRoutes.js";
-
-// import { connectDB } from "./lib/db.js";
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use("/api/auth",authRoutes);
-// app.use("/api/books",bookRoutes);
-
-// app.listen(PORT, () => {
-//     console.log(`Server is on port ${PORT}..`);
-//     connectDB();
-// });
-
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -29,19 +6,42 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
 import { connectDB } from "./lib/db.js";
-import { createServer } from "@vercel/node"; 
-// Import Vercel's serverless handler
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/books",bookRoutes);
 
-// Connect to the database before handling requests
-connectDB();
+app.listen(PORT, () => {
+    console.log(`Server is on port ${PORT}..`);
+    connectDB();
+});
 
-// Export the serverless handler for Vercel
-export default createServer(app);
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
+
+// import authRoutes from "./routes/authRoutes.js";
+// import bookRoutes from "./routes/bookRoutes.js";
+
+// import { connectDB } from "./lib/db.js";
+// import { createServer } from "@vercel/node"; 
+// // Import Vercel's serverless handler
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/books", bookRoutes);
+
+// // Connect to the database before handling requests
+// connectDB();
+
+// // Export the serverless handler for Vercel
+// export default createServer(app);
